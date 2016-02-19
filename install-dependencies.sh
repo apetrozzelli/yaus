4fa4d5f1d15e3b36e2e879aeb24ea6be00c5ae57
Alessandro Petrozzelli <apetrozzelli@gmail.com>
2016-02-20 00:41:56 +0100

    working on googletest as a submodule

---
+++ install-dependencies.sh
@@ -0,0 +1,28 @@
+echo "building dependencies..."
+
+git submodule init
+git submodule update
+
+cd dependencies
+
+echo "googletest"
+
+cd googletest
+
+export SHARED_LIB=OFF
+export STATIC_LIB=ON
+export CMAKE_PKG=OFF
+export BUILD_TYPE=debug
+#export VERBOSE_MAKE=true
+
+mkdir build || true
+mkdir build/googletest || true
+cd build/googletest
+
+cmake -Dgtest_build_samples=OFF \
+      -Dgmock_build_samples=OFF \
+      -Dgtest_build_tests=OFF \
+      -Dgmock_build_tests=OFF \
+      -DCMAKE_CXX_FLAGS=$CXX_FLAGS \
+      ../../googletest
+make
